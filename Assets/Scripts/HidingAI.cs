aa164d5b639648762d2aeb7379a8d825ac45137d
r3lek <AILD213@yahoo.com>
2018-10-25 00:17:21 -0700

    Navmesh, AI calling upon others, moving platform, jump/jetpack keys have been implemented

--- Assets/Scripts/HidingAI.cs
+++ Assets/Scripts/HidingAI.cs
@@ -36,21 +36,33 @@ public class HidingAI : MonoBehaviour
             if (GameObject.Find("Player") && GameObject.Find("Player").GetComponent<PlayerCharacter>()._health >= 0)
             {
                 Vector3 offset = GameObject.Find("Player").transform.position - transform.position;
+
                 float sqrLen = offset.sqrMagnitude;
-                if (sqrLen < closeDistance * closeDistance)
+                print("THE SQR LENGTH: " + sqrLen);
+
+                print("The other transform is close to me!");
+                //transform.LookAt(GameObject.Find("Player").transform.position);
+                if (sqrLen < closeDistance*closeDistance)
                 {
-                    print("The other transform is close to me!");
+                    print("The other transform is close to me! BY THIS AMMOUNT " + sqrLen);
                     transform.LookAt(GameObject.Find("Player").transform.position);
-                }
-            }
-            Ray ray = new Ray(transform.position, transform.forward);
-            RaycastHit hit;
-            GameObject hitObject = hit.transform.gameObject;
-                if (hitObject.GetComponent<PlayerCharacter>() && hitObject.GetComponent<PlayerCharacter>()._health >= 0)
-                {
+
+                    //since enemy2 can see player, call for help
+                    GameObject[] listEnemy1 = GameObject.FindGameObjectsWithTag("Target");
+                    foreach (GameObject enemy1 in listEnemy1)
+                    {
+                        //Debug.Log("ENEMY 1::::: " + enemy1.transform.position);
+                        float distFromE1toE3 = (enemy1.transform.position - transform.position).sqrMagnitude;
+                        if (distFromE1toE3 < 200 * closeDistance)
+                        {
+                            Debug.Log("YO ENEMY 1. TO THE RESCUE!!! ");
+                            enemy1.GetComponent<WanderingAnim>().agent.SetDestination(transform.position);
+                        }
+                    }
+
                     if (GameObject.Find("Player").transform.position != null)
                     {
-                        Debug.Log("The player position is " + GameObject.Find("Player").transform.position);
+                        //Debug.Log("The player position is " + GameObject.Find("Player").transform.position);
                     }
                     transform.LookAt(GameObject.Find("Player").transform.position);
                     //PlayerCharacter playerPosition = GetComponent<PlayerCharacter>();
@@ -60,10 +72,35 @@ public class HidingAI : MonoBehaviour
 
                         transform.LookAt(GameObject.Find("Player").transform.position);
                         _fireball = Instantiate(fireballPrefab) as GameObject;
-						_fireball.transform.position = transform.TransformPoint(0, 1.2f, 1 * 1.5f);
+                        _fireball.transform.position = transform.TransformPoint(0, 1.2f, 1 * 1.5f);
                         _fireball.transform.rotation = transform.rotation;
                     }
                 }
+
+            }
+            /* UNCOMMENT THIS FOR EXTREME MODE lol*/
+            //      Ray ray = new Ray(transform.position, transform.forward);
+            //      RaycastHit hit;
+            //      GameObject hitObject;
+            //          if (GameObject.Find("Player") && GameObject.Find("Player").GetComponent<PlayerCharacter>()._health >= 0)
+            //          {
+            ////              if (GameObject.Find("Player").transform.position != null)
+            ////              {
+            ////                  //Debug.Log("The player position is " + GameObject.Find("Player").transform.position);
+            ////              }
+            ////              transform.LookAt(GameObject.Find("Player").transform.position);
+            ////              //PlayerCharacter playerPosition = GetComponent<PlayerCharacter>();
+            ////              if (_fireball == null)
+            ////              {
+
+
+            ////                  transform.LookAt(GameObject.Find("Player").transform.position);
+            ////                  _fireball = Instantiate(fireballPrefab) as GameObject;
+            ////_fireball.transform.position = transform.TransformPoint(0, 1.2f, 1 * 1.5f);
+            ////                  _fireball.transform.rotation = transform.rotation;
+            ////              }
+            //          }
+            //  }
         }
         else
         {
--- Assets/WanderingAnim.cs
+++ Assets/WanderingAnim.cs
@@ -7,14 +7,16 @@ public class WanderingAnim : MonoBehaviour
     public float speed = 3.0f;
     public float obstacleRange = 2.0f;
     public float closeDistance = 15.0F; //distance between player and enemy
+    public Vector3 needsHelp;
 
     [SerializeField] private GameObject fireballPrefab;
     private GameObject _fireball;
     public Transform target;
     private bool _aliveAnim;
     private Animator _animator;
+    public Vector3 setDest;
 
-    private NavMeshAgent agent;
+    public NavMeshAgent agent;
 
     //positions of where to patrol
     Vector3[] patrolPositions = { new Vector3 { x = -34.0f, y = 1.0f, z = -25.0f },
@@ -56,16 +58,31 @@ public class WanderingAnim : MonoBehaviour
             _animator = GetComponent<Animator>();
             //transform.Translate(0, 0, speed * Time.deltaTime); //move forward
 
-            if (GameObject.Find("Player") && GameObject.Find("Player").GetComponent<PlayerCharacter>()._health >= 0)
+            if (GameObject.Find("Player") && GameObject.Find("Player").GetComponent<PlayerCharacter>()._health > 0)
             {
                 Vector3 offset = GameObject.Find("Player").transform.position - transform.position;
                 float sqrLen = offset.sqrMagnitude;
-                if (sqrLen < closeDistance * closeDistance)
+                if (sqrLen < closeDistance * closeDistance && GameObject.Find("Player") && GameObject.Find("Player").GetComponent<PlayerCharacter>()._health > 0)
                 {
                     //print("The other transform is close to me!");
                     //Debug.Log("THE PLAYERS Y VALUE: " + transform.position);
-                    transform.LookAt(GameObject.Find("Player").transform.position);
+                    //transform.LookAt(GameObject.Find("Player").transform.position);
                     agent.destination = GameObject.Find("Player").transform.position;
+                    if (GameObject.Find("Player").transform.position != null)
+                    {
+                        //Debug.Log("The player position is " + GameObject.Find("Player").transform.position);
+
+                    }
+                    transform.LookAt(GameObject.Find("Player").transform.position);
+                    //PlayerCharacter playerPosition = GetComponent<PlayerCharacter>();
+                    if (_fireball == null)
+                    {
+                        Debug.Log("Is shooting");
+                        transform.LookAt(GameObject.Find("Player").transform.position);
+                        _fireball = Instantiate(fireballPrefab) as GameObject;
+                        _fireball.transform.position = transform.TransformPoint(0, 1, 1 * 1.5f);
+                        _fireball.transform.rotation = transform.rotation;
+                    }
 
                 }
                 //has not seen me so just patrol
@@ -82,34 +99,36 @@ public class WanderingAnim : MonoBehaviour
 
             Ray ray = new Ray(transform.position, transform.forward);
             RaycastHit hit;
-            if (Physics.SphereCast(ray, 0.75f, out hit))
+
+            GameObject hitObject;
+                if (GameObject.Find("Player") && GameObject.Find("Player").GetComponent<PlayerCharacter>()._health > 0)
+                {
+      //              if (GameObject.Find("Player").transform.position != null)
+      //              {
+      //                  //Debug.Log("The player position is " + GameObject.Find("Player").transform.position);
+                        
+      //              }
+      //              transform.LookAt(GameObject.Find("Player").transform.position);
+      //              //PlayerCharacter playerPosition = GetComponent<PlayerCharacter>();
+      //              if (_fireball == null)
+      //              {
+						//Debug.Log("Is shooting");
+      //                  transform.LookAt(GameObject.Find("Player").transform.position);
+      //                  _fireball = Instantiate(fireballPrefab) as GameObject;
+      //                  _fireball.transform.position = transform.TransformPoint(0, 1, 1 * 1.5f);
+      //                  _fireball.transform.rotation = transform.rotation;
+      //              }
+                }
+            else
             {
-                GameObject hitObject = hit.transform.gameObject;
-                if (hitObject.GetComponent<PlayerCharacter>() && hitObject.GetComponent<PlayerCharacter>()._health >= 0)
-                {
-                    if (GameObject.Find("Player").transform.position != null)
-                    {
-                        //Debug.Log("The player position is " + GameObject.Find("Player").transform.position);
-                    }
-                    transform.LookAt(GameObject.Find("Player").transform.position);
-                    //PlayerCharacter playerPosition = GetComponent<PlayerCharacter>();
-                    if (_fireball == null)
-                    {
-						Debug.Log("Is shooting");
-                        transform.LookAt(GameObject.Find("Player").transform.position);
-                        _fireball = Instantiate(fireballPrefab) as GameObject;
-                        _fireball.transform.position = transform.TransformPoint(0, 1, 1 * 1.5f);
-                        _fireball.transform.rotation = transform.rotation;
-                    }
-                }
-                else if (hit.distance < obstacleRange)
-                {
-                    float angle = Random.Range(-110, 110);
-                    //transform.Rotate(0, angle, 0);
-                }
+                //int randomPost = Random.Range(0,10);
+                if (agent.remainingDistance < 0.5f)
+                    GotoNextPoint();
             }
 
-          
+
+
+
         }
         else
         {
@@ -122,5 +141,9 @@ public class WanderingAnim : MonoBehaviour
     public void SetAlive(bool alive)
     {
         _aliveAnim = alive;
+    }
+
+    public void setActive(bool active) {
+        
     }
 }
