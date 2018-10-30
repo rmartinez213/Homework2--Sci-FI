4b3820b584d53fa3fc7c7edd6bb6dd38ec1885c9
rmartinez213 <rmartinez213@yahoo.com>
2018-10-29 19:59:31 -0700

    Mereged with miguel stuff and working door animation

---
+++ Assets/Scripts/HoldCharacter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldCharacter : MonoBehaviour
+{
+
+    //on collision
+    void OnTriggerEnter(Collider col)
+    {
+        col.transform.parent = gameObject.transform;
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        col.transform.parent = null;
+    }
+
+
+}
