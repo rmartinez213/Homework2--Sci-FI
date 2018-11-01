ab22bf8b8a0f7f3f69f872c5fc46c54faf17c962
r3lek <AILD213@yahoo.com>
2018-10-31 17:54:59 -0700

    Added health bar on top

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -1,26 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerCharacter : MonoBehaviour
 {
-	public int _health = 0;
+	public int _health = 150;
 
 	//Attach all game objects
     public GameObject spawn;
     public GameObject HealthObject;
     public GameObject HealthObject2;
 	public GameObject KeyObject;
-	   
+
+
+    public Image currentHealthBar;
+    public Text ratioText;
+
 
     void Start()
     {
-        _health = 5;
+        _health = 100;
 
     }
 
     public void StartAgain()
     {
-        _health = 5;
+        _health = 100;
 
         this.gameObject.transform.rotation = spawn.transform.rotation;
         this.gameObject.transform.position = spawn.transform.position;
@@ -52,7 +57,12 @@ public class PlayerCharacter : MonoBehaviour
 
     public void Hurt(int damage)
     {
+        
         _health -= damage;
+        float ratio = _health / 100.0f;
+        print("The ratio when taking damage: "+ ratio);
+        currentHealthBar.rectTransform.localScale = new Vector3(ratio,1,1);
+        ratioText.text = (_health).ToString();
 
         if (_health <= 0)
         {
